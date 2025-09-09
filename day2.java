96925c32fa16cdf0e12885e7a31afa75385004eb
Aco Muh. Arrafi Ahmad <acoarrafi123@gmail.com>
2025-09-09 15:26:21 +0800

    Create day2.java

---
+++ day2.java
@@ -0,0 +1,23 @@
+public class Day2 {
+    public static void main(String[] args) {
+        
+        //println() Menampilkan teks ke layar lalu pindah baris (newline).
+        System.out.println("aco");
+        System.out.println("arrafi");
+        System.out.println(""); //pindah baris
+        
+        //print() Menampilkan teks ke layar tanpa pindah baris setelahnya
+        System.out.print("aco");
+        System.out.print("arrafi");
+        System.out.println(""); //pindah baris
+        
+        //printf() Digunakan untuk menampilkan teks dengan format tertentu (formatting).
+        //Misalnya menampilkan angka desimal, pecahan, atau rata kanan/kiri.
+        int umur = 20;
+        double nilai = 95.75;
+
+        System.out.printf("Umur saya %d tahun%n", umur);
+        System.out.printf("Nilai saya adalah %.2f%n", nilai);
+        
+    }
+}
