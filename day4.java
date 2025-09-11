e2d3c5c4285c0e3ea9e972658e8ee9d54954ddbb
Aco Muh. Arrafi Ahmad <acoarrafi123@gmail.com>
2025-09-11 13:10:35 +0800

    Add files via upload

---
+++ day4.java
@@ -0,0 +1,26 @@
+public class day4 {
+    public static void main(String[] args) {
+        // untuk pindah baris (\n)
+        System.out.println("Baris pertama.\nBaris kedua.");
+        
+        // Menggunakan tab (\t) 
+        System.out.println("Nama:\tARRAFI");
+        System.out.println("Umur:\t21");
+        
+        // Menampilkan double quote (\") untuk menampilkan tanda kutip
+        System.out.println("Dia berkata, \"Hello World!\"");
+        // menampilkan one quote (\') untuk tanda kutip tunggal
+        System.out.println("saya bilang, \'MEYLANE\'");
+        // Menampilkan backslash (\\) atau garis miring
+        System.out.println("Path: C:\\Program Files\\Java");
+        
+        // Menggunakan carriage return (\r) untuk timpa teks sebelumnya
+        System.out.println("SAYA.\rTertimpa");  
+        
+        // Menggunakan backspace (\b) itu huruf hilang di belakang
+        System.out.println("Java\bScript"); // Output: JavaScript (tanpa 'a') menjadi JavScript
+        
+        // Form feed (\f) - biasanya tidak terlihat di konsol
+        System.out.println("Halaman 1 \f Halaman 2"); //tanda panah
+    }
+}
