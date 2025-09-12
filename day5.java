3028558cb363df66de8b3ca3f9e806ca7875344a
Aco Muh. Arrafi Ahmad <acoarrafi123@gmail.com>
2025-09-12 20:19:10 +0800

    Add files via upload

---
+++ day5.java
@@ -0,0 +1,15 @@
+
+public class day5 {
+    public static void main(String[] args) {
+        int umur = 18;//32 bit
+     byte tinggi = 2; //8 bit
+        short jarakkota = 10000; //16 bit
+        long panjangtali = 1000000000; //64 bit
+        
+        System.out.printf("Umur saya %d tahun%n", umur);
+          System.out.printf("Tinggi badan %d Meter %n", tinggi);
+        System.out.printf("JARAK KOTA MEYLANE itu %d %n", jarakkota);
+        System.out.printf("Jumlah Search %d %n", panjangtali);
+    }
+      
+}
