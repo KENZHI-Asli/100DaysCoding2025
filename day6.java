19cff22e9d940189fbc0dc23b5d436daa7787e33
Aco Muh. Arrafi Ahmad <acoarrafi123@gmail.com>
2025-09-13 11:42:55 +0800

    Add files via upload

---
+++ day6.java
@@ -0,0 +1,9 @@
+
+public class day6 {
+    public static void main(String[] args) {
+        double a = 95.75; 
+        float b = 300.0f; // harus suffix f 
+        System.out.println("harga Rp."+ b+"juta");
+        System.out.println("Nilai double "+ a);
+    }
+}
