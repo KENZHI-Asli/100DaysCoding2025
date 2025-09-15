5efd3bb095aab9e1f9600df78257779b683e4850
Aco Muh. Arrafi Ahmad <acoarrafi123@gmail.com>
2025-09-15 15:47:11 +0800

    Add files via upload

---
+++ day8.java
@@ -0,0 +1,10 @@
+public class day8 {
+    public static void main(String[] args) {      
+        String nama = "Aco Arrafi";
+        String hobi = "Maingame";
+       
+        System.out.println("nama saya " + nama); //teks itu yang dalam petik dua "ini"
+        System.out.println("hobi saya " + hobi);
+    }
+}
+
