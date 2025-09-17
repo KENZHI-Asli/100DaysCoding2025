8ca30bfd9aa4924f8bb0cbaf62e4bdc61c08e918
Aco Muh. Arrafi Ahmad <acoarrafi123@gmail.com>
2025-09-17 20:35:12 +0800

    Add files via upload

---
+++ day10.java
@@ -0,0 +1,10 @@
+public class day10 {
+    public static void main(String[] args) {
+        final boolean status = true;
+        final String suatu = "Sesuatu";
+        final byte nilai = 100;       
+        System.out.println("Status hp: " + status);
+        System.out.println("Tambah: " + suatu);
+        System.out.println("NILAI: " + nilai);
+    }
+}
