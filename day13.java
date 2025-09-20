e45ca56eae0fa3ec9b751f6c480a05ac6394c870
Aco Muh. Arrafi Ahmad <acoarrafi123@gmail.com>
2025-09-20 12:42:35 +0800

    Add files via upload

---
+++ day13.java
@@ -0,0 +1,12 @@
+
+public class day13 {
+    public static void main(String[] args) {
+        double warisan = 120.000;
+        int cipung_mariati_rifai = 3;
+        int nim = 48;
+        double admin = 120.000*0.48;
+        double hasilakhir = warisan-admin;
+        double hasilbagi = hasilakhir/cipung_mariati_rifai;
+        System.out.println("hasil bagi \n"+ hasilbagi);
+    }
+}
