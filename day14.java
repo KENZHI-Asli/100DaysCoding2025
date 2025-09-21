9f440e12c519d1cb364dbc416e4c2f48b6db47e8
Aco Muh. Arrafi Ahmad <acoarrafi123@gmail.com>
2025-09-21 18:32:54 +0800

    Add files via upload

---
+++ day14.java
@@ -0,0 +1,11 @@
+
+public class day14 {
+    public static void main(String[] args) {
+        String INT = "123";
+        String BOOLEAN = "true";
+        int angka = Integer.parseInt(INT);
+        boolean bolean = Boolean.parseBoolean(BOOLEAN);        
+        System.out.println("string ke integer: "+ angka);
+        System.out.println("string ke boolean: "+ bolean);
+    }
+}
