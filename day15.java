47563c530ced78ac4302a5fcef6054e87791eefd
Aco Muh. Arrafi Ahmad <acoarrafi123@gmail.com>
2025-09-22 18:22:39 +0800

    Add files via upload

---
+++ day15.java
@@ -0,0 +1,9 @@
+
+public class day15 {
+    public static void main(String[] args) {
+        int a = 150;
+        int b = 50;
+        System.out.println("Penjumlahan: "+(a+b));
+        System.out.println("Pengurangan: "+(a-b));
+    }
+}
