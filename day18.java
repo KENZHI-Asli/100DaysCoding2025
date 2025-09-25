7bef457b5740f47bc3395b3e2dd44c49ad2a7e27
Aco Muh. Arrafi Ahmad <acoarrafi123@gmail.com>
2025-09-25 19:27:04 +0800

    Add files via upload

---
+++ day18.java
@@ -0,0 +1,8 @@
+
+public class day18 {
+    public static void main(String[] args) {
+        byte input = 100;
+        double nilai = input;
+        System.out.println(input+" kecil ke "+nilai+" Besar ");
+    }
+}
