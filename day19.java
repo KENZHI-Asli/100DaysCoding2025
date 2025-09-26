882fa16fd129efd720b328d79852367932272b80
Aco Muh. Arrafi Ahmad <acoarrafi123@gmail.com>
2025-09-26 20:26:23 +0800

    Add files via upload

---
+++ day19.java
@@ -0,0 +1,8 @@
+
+public class day19 {
+    public static void main(String[] args) {
+        double a = 120.8;
+        byte b = (byte) a;
+        System.out.println(a+" besar -> kecil = "+b);
+    }
+}
