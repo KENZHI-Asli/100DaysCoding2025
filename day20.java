2bbca74c86759f05fcca52ea3d56d26d38578d9d
Aco Muh. Arrafi Ahmad <acoarrafi123@gmail.com>
2025-09-27 19:15:07 +0800

    Add files via upload

---
+++ day20.java
@@ -0,0 +1,16 @@
+
+public class day20 {
+    public static void main(String[] args) {
+        int a = 200;
+        double b = 82.0;
+        String nilai = Integer.toString(a);
+        String hasil = Double.toString(b);
+        String baru = ""+a;
+        String lama = ""+b;
+        System.out.println(nilai);
+        System.out.println(hasil+"\n");
+        System.out.println(baru);
+        System.out.println(lama);
+    }
+  
+}
