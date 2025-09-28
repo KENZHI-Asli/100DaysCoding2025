e8b061f0caa9e48fe5612e81ff8969128356f345
Aco Muh. Arrafi Ahmad <acoarrafi123@gmail.com>
2025-09-28 20:13:27 +0800

    Add files via upload

---
+++ day21.java
@@ -0,0 +1,14 @@
+
+public class day21 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 40;
+        
+        a = a+b;
+        b = a-b;
+        a = a-b;
+        System.out.println("nilai a = "+a);
+        System.out.println("nilai b = "+b);
+    }
+ 
+}
