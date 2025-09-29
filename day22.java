5b6c273ab9f36b019ef4e77383a5f1959798be6c
Aco Muh. Arrafi Ahmad <acoarrafi123@gmail.com>
2025-09-29 18:46:35 +0800

    Add files via upload

---
+++ day22.java
@@ -0,0 +1,8 @@
+
+public class day22 {
+    public static void main(String[] args) {
+        int luas = 5;
+        int hasil = luas*luas;
+        System.out.println("Hasil luas persegi = "+hasil);
+    }
+}
