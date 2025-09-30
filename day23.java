63b9ae8783f9d1c6192525d28a777f57ce7f4a25
Aco Muh. Arrafi Ahmad <acoarrafi123@gmail.com>
2025-09-30 18:37:08 +0800

    Add files via upload

---
+++ day23.java
@@ -0,0 +1,9 @@
+
+public class day23 {
+    public static void main(String[] args) {
+        int panjang = 10;
+        int lebar = 5;
+        int hasil = panjang*lebar;
+        System.out.println("hasil luas persegi panjang = "+hasil);
+    }
+}
