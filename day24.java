989dba96e90b11ab174e7c674c3a906bff07502f
Aco Muh. Arrafi Ahmad <acoarrafi123@gmail.com>
2025-10-01 18:54:56 +0800

    Add files via upload

---
+++ day24.java
@@ -0,0 +1,8 @@
+
+public class day24 {
+    public static void main(String[] args) {
+        double jarijari = 10.1;
+        double luas = Math.PI*jarijari*jarijari;
+        System.out.println("jumlah luas lingkaran = "+luas);
+    }
+}
