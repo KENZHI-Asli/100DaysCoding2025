e40cc39f636640749b1756b91b4434c7556ff77a
Aco Muh. Arrafi Ahmad <acoarrafi123@gmail.com>
2025-10-02 19:00:46 +0800

    Add files via upload

---
+++ day25.java
@@ -0,0 +1,15 @@
+
+public class day25 {
+    public static void main(String[] args) {
+        int a = 8;
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(++a+"\n");
+        int b = 12;
+        System.out.println(b);
+        System.out.println(b--);
+        System.out.println(b);
+        System.out.println(--b);
+    }
+}
