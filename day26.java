8d8015a02985666008a5fde04136b3c5089a2fe9
Aco Muh. Arrafi Ahmad <acoarrafi123@gmail.com>
2025-10-03 20:20:15 +0800

    Add files via upload

---
+++ day26.java
@@ -0,0 +1,17 @@
+
+public class day26 {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("nilai a = "+a);
+        a += 5;
+        System.out.println("hasil +=: "+a);
+        a -= 3;
+        System.out.println("hasil -=: "+a);
+        a *= 4;
+        System.out.println("hasil *=: "+a);
+        a /= 5;
+        System.out.println("hasil /=: "+a);
+        a %= 4;
+        System.out.println("hasil %=: "+a);
+    }
+}
