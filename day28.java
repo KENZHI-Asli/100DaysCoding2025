f93d1f01a4b3200a1f3b1fbdd79eb1da91ec06b3
Aco Muh. Arrafi Ahmad <acoarrafi123@gmail.com>
2025-10-05 19:53:26 +0800

    Add files via upload

---
+++ day28.java
@@ -0,0 +1,11 @@
+public class day28 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 10;
+        System.out.println("Apakah a == b? " + (a == b)); // false
+        System.out.println("Apakah a == c? " + (a == c)); // true
+        System.out.println("Apakah a != b? " + (a != b)); // true
+        System.out.println("Apakah b != c? " + (b != c)); // true     
+    }
+}
