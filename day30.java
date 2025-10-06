1f57c68c5f2afa1d51875b74fa2e467de8905403
Aco Muh. Arrafi Ahmad <acoarrafi123@gmail.com>
2025-10-07 04:06:52 +0800

    Add files via upload

---
+++ day30.java
@@ -0,0 +1,12 @@
+
+public class day30 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+        int c = 30;
+        System.out.println("kurang dari :  "+a+" <= "+b+(a <= b));
+        System.out.println("lebih dari :  "+a+" >= "+b+(a >= b));
+        System.out.println("kurang dari :  "+b+" <= "+c+(b <= c));
+        System.out.println("lebih dari :  "+c+" >= "+b+(c >= b));
+    }
+}
