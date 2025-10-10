dd68f4421bfb51d9980e093dbcf3b7b38e51a9f3
Aco Muh. Arrafi Ahmad <acoarrafi123@gmail.com>
2025-10-10 20:33:15 +0800

    Add files via upload

---
+++ day33.java
@@ -0,0 +1,8 @@
+public class day33 {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("false = "+ !b);
+        System.out.println("true = "+ !a);
+    }    
+}
