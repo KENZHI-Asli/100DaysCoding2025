306121cebadfd0c457243555b68f17ac7ac7006d
Aco Muh. Arrafi Ahmad <acoarrafi123@gmail.com>
2025-10-12 19:40:06 +0800

    Add files via upload

---
+++ day35.java
@@ -0,0 +1,12 @@
+
+public class day35 {
+    public static void main(String[] args) {
+        int nilai = 80;
+
+        if (nilai >= 70) {
+            System.out.println("Selamat, kamu lulus!");
+        }else{
+            System.out.println("tidak lulus");
+    }
+    }
+}
