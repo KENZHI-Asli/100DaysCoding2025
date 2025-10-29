077f2cb386ef72630687f26476ea844f6138e84f
Aco Muh. Arrafi Ahmad <acoarrafi123@gmail.com>
2025-10-29 18:20:59 +0800

    Add files via upload

---
+++ day52.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class day52 {
+    public static void main(String[] args) {
+        Scanner up = new Scanner(System.in);
+        System.out.print("masukkan nilai: ");
+        int angka = up.nextInt();
+        while (angka <= 3){
+            System.out.println("hasilnya "+angka);
+           angka++;
+        }
+    }
+}
