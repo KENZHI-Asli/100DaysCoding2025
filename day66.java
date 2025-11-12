3dc02fd98fac974baffd99df5e8a6a00e9e656a5
Aco Muh. Arrafi Ahmad <acoarrafi123@gmail.com>
2025-11-12 18:56:23 +0800

    Add files via upload

---
+++ day66.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class day66 {
+    public static void main(String[] args) {
+        Scanner up = new Scanner(System.in);
+        System.out.print("jumlah: ");
+        int a = up.nextInt();
+        for (int i = 0; i < a; i++) {
+            System.out.println("*");
+        }
+    }
+}
